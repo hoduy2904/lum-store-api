1e56200a333ba18aa530c56988f9e663a95341d5
Duy Ho <duyho@Duys-MacBook-Air.local>
2026-05-06 23:24:20 +0700

    add type carousel

---
+++ LumStoreAPI.Infrastructure/Migrations/20260506162333_AddTypeCarouselItem.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LumStoreAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypeCarouselItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "CTAImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "CTAImages");
+        }
+    }
+}
