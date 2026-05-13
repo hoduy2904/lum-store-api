d2a33422d6eb9e3645875525bb1aef6178ece671
Duy Ho <duyho@Duys-MacBook-Air.local>
2026-05-13 23:30:39 +0700

    resolve seller

--- LumStoreAPI.SDK/Shiprelay/Handlers/ShiprelayRateService.cs
+++ LumStoreAPI.SDK/Shiprelay/Handlers/ShiprelayRateService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Http.Json;
+using LumStoreAPI.Core.Interfaces.Repositories;
 using LumStoreAPI.SDK.Extensions;
 using LumStoreAPI.SDK.Shiprelay.Interfaces;
 using LumStoreAPI.SDK.Shiprelay.Models;
@@ -8,13 +9,16 @@ using LumStoreAPI.SDK.Shiprelay.Models.Requests;
 namespace LumStoreAPI.SDK.Shiprelay.Handlers;
 
 public class ShiprelayRateService(
-    IHttpClientFactory httpClientFactory
+    IHttpClientFactory httpClientFactory,
+    IIntegrationConfigRepository integrationConfigRepository
 ) : IShiprelayRateService
 {
     private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+    private readonly IIntegrationConfigRepository _integrationConfigRepository = integrationConfigRepository;
     public async Task<ShiprelayPagedResponse<RateOption>> GetRates(RateRequest rateRequest)
     {
-        rateRequest.ResellerId = "f8c2d4d7-1cef-4b7a-a7bd-697f59673722";
+        var config = await _integrationConfigRepository.GetConfigByTypeAsync(Core.Models.Enums.IntegrationType.Shiprelay);
+        rateRequest.ResellerId = config?.ResellerId ?? "";
 
         var shiprelayClient = _httpClientFactory.CreateShiprelayClient();
         var response = await shiprelayClient.PostAsJsonAsync($"rates/calculate", rateRequest);
