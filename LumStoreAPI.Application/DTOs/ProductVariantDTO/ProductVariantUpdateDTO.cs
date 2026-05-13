7cdb1933c40032b7759df26729c891124f80ccd6
Duy Ho <duy.ho@createstudios.com.au>
2026-05-13 18:47:23 +0700

    fix update variant

--- LumStoreAPI.Application/DTOs/ProductVariantDTO/ProductVariantUpdateDTO.cs
+++ LumStoreAPI.Application/DTOs/ProductVariantDTO/ProductVariantUpdateDTO.cs
@@ -7,6 +7,6 @@ public record class ProductVariantUpdateDTO
     public string SKU { get; set; } = default!;
     public string UPC { get; set; } = default!;
     public Guid[] Images { get; set; } = [];
-    public int ColorId { get; set; }
+    public int? ColorId { get; set; }
     public string VariantName { get; set; } = default!;
 }
