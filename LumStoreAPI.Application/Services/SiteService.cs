ada7b264090c8517712dbb8289a58ced32d1b63b
thailevnaiss <thaile.vn.ai.ss@gmail.com>
2026-05-23 14:50:24 +0700

    fix site navigations

--- LumStoreAPI.Application/Services/SiteService.cs
+++ LumStoreAPI.Application/Services/SiteService.cs
@@ -37,10 +37,35 @@ public class SiteService(
                   });
               }, cache => cache.Dependencies(d => d.Nodes().NodeOrder()).Key("navigations"));
 
-        var imageGuids = navigations.SelectMany(x => x.OgImage).ToArray();
+        var navNodeIds = navigations.Select(x => (int?)x.NodeID).ToArray();
+        var children = navNodeIds.Length == 0
+            ? []
+            : await _pageRetrieveContext.GetPagesAsync<DocumentPage>(query =>
+              {
+                  query.Where(x => navNodeIds.Contains(x.Node.ParentNodeID) && x.Node.ClassName == "Pages.ProductCategory")
+                  .Select(x => new DocumentPage
+                  {
+                      DocumentName = x.DocumentName,
+                      NodeID = x.NodeID,
+                      Node = new DocumentNode
+                      {
+                          NodeID = x.NodeID,
+                          RelativeUrl = x.Node.RelativeUrl,
+                          ParentNodeID = x.Node.ParentNodeID,
+                          ClassName = x.Node.ClassName
+                      },
+                      OgImage = x.OgImage,
+                      OgTitle = x.OgTitle,
+                      OgDescription = x.OgDescription
+                  });
+              }, cache => cache.Dependencies(d => d.Nodes().NodeOrder()).Key("navigations_children"));
+
+        var allPages = navigations.Concat(children).ToList();
+
+        var imageGuids = allPages.SelectMany(x => x.OgImage).ToArray();
         var images = (await _mediaService.GetMediaItemsAsync(imageGuids)).ToDictionary(x => x.FileID, x => x.FileURL);
 
-        var navItems = navigations.MappingTree()
+        var navItems = allPages.MappingTree()
         .Select(x => new NavItem(x, images));
 
         return navItems;
