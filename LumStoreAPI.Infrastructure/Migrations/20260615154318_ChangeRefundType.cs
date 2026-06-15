d61992e86c861795f6d2bb401ef74a2d3ae3a686
Duy Ho <duyho@Duys-MacBook-Air.local>
2026-06-15 22:45:34 +0700

    change type

---
+++ LumStoreAPI.Infrastructure/Migrations/20260615154318_ChangeRefundType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LumStoreAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRefundType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "RefundAmount",
+                table: "OrderReturns",
+                type: "bigint",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "RefundAmount",
+                table: "OrderReturns",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+    }
+}
